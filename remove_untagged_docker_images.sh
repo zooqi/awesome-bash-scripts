ad5c008b4efc5f71588844b540a45d18f816ffad
Judge <zooqih@gmail.com>
2020-06-28 21:48:47 +0800

    fix

--- remove_untagged_docker_images.sh
+++ remove_untagged_docker_images.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 GREP_RESULT=$(docker image ls -a | grep "<none>")
-test -z "${GREP_RESULT}" || echo ${GREP_RESULT} | awk '{print $3}' | xargs docker rmi
+test -z "${GREP_RESULT}" || echo "${GREP_RESULT}" | awk '{print $3}' | xargs docker rmi
